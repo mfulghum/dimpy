c6425c67862501690a17a34af30dfe0f9f2a677a
Matt Fulghum <mfulghum@gmail.com>
2016-06-09 03:23:47 -0700

    Add dimension and scale parameters to dimensioned array

--- dimpy/array.py
+++ dimpy/array.py
@@ -6,13 +6,17 @@ Copyright 2016, Matthew Fulghum [mfulghum]
 """
 
 import numpy as np
+import units as unit_check
 
 class array(np.ndarray):
     def __new__(cls, input_array, units=None):
         obj = np.asarray(input_array).view(cls)
         obj.units = units
+        obj.power, obj.dimensions = unit_check.check_dimensionality(units)
         return obj
 
     def __array_finalize__(self, obj):
         if obj is None: return
         self.units = getattr(obj, 'units', '1')
+        self.power = getattr(obj, 'power', 0)
+        self.dimensions = getattr(obj, 'dimensions', np.zeros(7, dtype=np.int8))
--- dimpy/units.py
+++ dimpy/units.py
@@ -1,23 +1,17 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Jun 05 17:56:27 2016
+Unit dimensioning checks
+Copyright 2016, Matthew Fulghum [mfulghum]
 
-@author: Matthew Fulghum
+9 June 2016 - Initial commit.
 """
-
-import dimpy
-import re
-
-vel = dimpy.array([1.0, 2.0, 3.0], units='m/s')
-
-unit_string = 'm^2/s'
-
-unit_regex = re.compile('([a-zA-Z]+)(?:\^(-?[0-9]+)|)')
-units = [{'unit':unit_power[0], 'power':(int(unit_power[1]) if unit_power[1] else 1) * (-1 if side else 1)} for side,units in enumerate(unit_string.split('/')) for unit_power in unit_regex.findall(units)]
 
 import sqlite3
+import re
+import numpy as np
+import glob
+import os
 
-db = sqlite3.connect('dimpy/units.db')
+db = sqlite3.connect(os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/units.db'))
 cursor = db.cursor()
 
 unit_regex = re.compile('([a-zA-Z]+)(?:\^(-?[0-9]+)|)')
@@ -41,6 +35,7 @@ def check_dimensionality(unit_string):
         JOIN prefixes
         WHERE unit=:unit
         '''
+
     dimensionality = np.sum([np.array(cursor.execute(query, unit).fetchone()[1:]) * unit['power'] for unit in units], axis=0)
     return dimensionality[0], dimensionality[1:]
 
