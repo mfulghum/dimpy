c745fbc2db22c12b3819003f2704fb972e08589d
mfulghum <matt.fulghum@oculus.com>
2016-06-10 11:33:11 -0700

    Updated the dimension check

--- dimpy/array.py
+++ dimpy/array.py
@@ -3,6 +3,7 @@ Dimensioned array extension to numpy
 Copyright 2016, Matthew Fulghum [mfulghum]
 
 5 June 2016 - Initial commit.
+9 June 2016 - Added checks for dimensionality.
 """
 
 import re
@@ -11,7 +12,7 @@ import numpy as np
 import units as unit_check
 
 class data(np.ndarray):
-    def __new__(cls, input_array, units='1'):
+    def __new__(cls, input_array, units=''):
         obj = np.asarray(input_array).view(cls)
         obj.units = units
         obj.power, obj.dimensions = unit_check.check_dimensionality(units)
@@ -19,7 +20,7 @@ class data(np.ndarray):
 
     def __array_finalize__(self, obj):
         if obj is None: return
-        self.units = getattr(obj, 'units', '1')
+        self.units = getattr(obj, 'units', '')
         self.power = getattr(obj, 'power', 0)
         self.dimensions = getattr(obj, 'dimensions', np.zeros(7, dtype=np.int8))
 
@@ -33,16 +34,27 @@ class data(np.ndarray):
         ufunc_type = str(context[0]).split("'")[1]
         num_inputs = context[0].nin
 
-        if ufunc_type in ['add', 'subtract', 'logaddexp', 'logaddexp2',
+        if ufunc_type in ['add', 'subtract', 'logaddexp', 'logaddexp2', 'mod', 'fmod', 'remainder',
                           'greater', 'greater_equal', 'equal', 'less_equal', 'less', 'not_equal']:
+            # These operations require dimensions to be consistent between operands
             assert np.all(context[1][0].dimensions == context[1][1].dimensions)
-        elif ufunc_type in ['sin', 'cos', 'tan', 'arcsin', 'arccos', 'arctan']:
+        elif ufunc_type in ['sin', 'cos', 'tan',
+                            'arcsin', 'arccos', 'arctan', 'arctan2',
+                            'sinh', 'cosh', 'tanh',
+                            'arcsinh', 'arccosh', 'arctanh',
+                            'deg2rad', 'rad2deg',
+                            'exp', 'exp2', 'log', 'log2', 'log10',
+                            'expm1', 'log1']:
+            # These operations require that the inputs be dimensionless
             assert np.all(context[1][0].dimensions == 0)
-
-        print('In __array_prepare__:')
-        print('   ufunc: %s' % ufunc_type)
-        print('   data: %s' % str(context[1]))
-        # then just call the parent
+        elif ufunc_type in ['sqrt']:
+            # The result of a sqrt operation must have integer dimension powers
+            assert np.all((context[1][0].dimensions % 2) == 0)
+        elif ufunc_type in ['power']:
+            # The result of a power operation must result in integer dimension powers
+            assert np.all(not isinstance(context[1][1], type(self)) or context[1][1].dimensions == 0)
+            assert np.all(context[1][1] == np.round(np.asarray(context[1][1])))
+            
         return np.ndarray.__array_prepare__(self, out_arr, context)
 
     def __array_wrap__(self, out_arr, context=None):
--- dimpy/units.py
+++ dimpy/units.py
@@ -3,12 +3,12 @@ Unit dimensioning checks
 Copyright 2016, Matthew Fulghum [mfulghum]
 
 9 June 2016 - Initial commit.
+10 June 2016 - Improved speed of the dimension checks
 """
 
 import sqlite3
 import re
 import numpy as np
-import glob
 import os
 
 db = sqlite3.connect(os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/units.db'))
@@ -16,25 +16,28 @@ cursor = db.cursor()
 
 unit_regex = re.compile('([a-zA-Z]+)(?:\^(-?[0-9]+)|)')
 
+unit_query = '''
+    SELECT
+      TRIM(prefix) || TRIM(symbol) AS unit,
+      units.power + prefixes.power AS power,
+      units.length,
+      units.mass,
+      units.time,
+      units.current,
+      units.temperature,
+      units.amount_of_substance,
+      units.luminous_intensity
+    FROM units
+    JOIN prefixes
+    WHERE unit=:unit
+    GROUP BY unit
+    '''
+
 def check_dimensionality(unit_string):
     units = [{'unit':unit_power[0], 'power':(int(unit_power[1]) if unit_power[1] else 1) * (-1 if side else 1)}
-             for side,units in enumerate(unit_string.split('/')) for unit_power in unit_regex.findall(units)]
-
-    query = '''
-        SELECT
-          CASE WHEN prefix=' ' THEN '' ELSE prefix END || symbol AS unit,
-          units.power + prefixes.power,
-          units.length,
-          units.mass,
-          units.time,
-          units.current,
-          units.temperature,
-          units.amount_of_substance,
-          units.luminous_intensity
-        FROM units
-        JOIN prefixes
-        WHERE unit=:unit
-        '''
-
-    dimensionality = np.sum([np.array(cursor.execute(query, unit).fetchone()[1:]) * unit['power'] for unit in units], axis=0)
+             for side,units in enumerate(unit_string.strip().split('/')) for unit_power in unit_regex.findall(units)]
+    if not units:
+        return 0, np.zeros(7, dtype=np.int8)
+    
+    dimensionality = np.sum([np.array(cursor.execute(unit_query, unit).fetchone()[1:]) * unit['power'] for unit in units], axis=0).astype(np.int8)
     return dimensionality[0], dimensionality[1:]
