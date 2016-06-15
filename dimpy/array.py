Fixed some floating point precision issues in the testing suite

dimpy/array.py
@@ -20,9 +20,6 @@ class data(np.ndarray):
         if obj is None: 
             return
         self.dimensions = getattr(obj, 'dimensions', np.zeros(7, dtype=np.int8))
-        print('array finalize:')
-        print(repr(obj))
-        print('')
 
     def __getitem__(self, item):
         return self.__class__(super(data, self).__getitem__(item), dimensions=self.dimensions)
@@ -33,11 +30,6 @@ class data(np.ndarray):
 
     def __array_prepare__(self, out_arr, context=None):
         ufunc_type = str(context[0]).split("'")[1]
-
-        print('array prepare:')
-        print(repr(out_arr))
-        print(repr(context))
-        print('')
 
         try:
             if ufunc_type in ['add', 'subtract', 'mod', 'fmod', 'remainder',
@@ -75,40 +67,32 @@ class data(np.ndarray):
         ufunc_type = str(context[0]).split("'")[1]
         out_arr = np.asarray(out_arr).view(self.__class__)
 
-        print('array wrap:')
-        print(repr(out_arr))
-        print(repr(context))
-        print('')
-
-        try:
-            if ufunc_type in ['multiply']:
-                # The result should add the dimensions of the two operands together
-                left_operand, right_operand = context[1][:2]
-                out_arr.dimensions = (left_operand.dimensions if isinstance(left_operand, type(self)) else 0) + \
-                                     (right_operand.dimensions if isinstance(right_operand, type(self)) else 0)
-            elif ufunc_type in ['divide', 'true_divide', 'floor_divide']:
-                # The result should subtract the dimensions of the second operand from the first
-                left_operand, right_operand = context[1][:2]
-                out_arr.dimensions = (left_operand.dimensions if isinstance(left_operand, type(self)) else 0) - \
-                                     (right_operand.dimensions if isinstance(right_operand, type(self)) else 0)
-            elif ufunc_type in ['power']:
-                # The result should have its dimensions multiplied by the exponent
-                left_operand, right_operand = context[1][:2]
-                out_arr.dimensions = left_operand.dimensions * np.asarray(right_operand)
-            elif ufunc_type in ['sqrt']:
-                # The result should have halved dimension powers
-                out_arr.dimensions = context[1][0].dimensions / 2
-            elif ufunc_type in ['square']:
-                # The result should have doubled dimension powers
-                out_arr.dimensions = context[1][0].dimensions * 2
-            elif ufunc_type in ['greater', 'greater_equal', 'equal', 'less_equal', 'less', 'not_equal']:
-                # Equality checks should be dimensionless
-                out_arr.dimensions = np.zeros_like(context[1][0].dimensions)
-            else:
-                # Other ufuncs will just return the left operand's dimensions
-                out_arr.dimensions = context[1][0].dimensions
-        except AssertionError, err:
-            raise DimensionError(err)
+        if ufunc_type in ['multiply']:
+            # The result should add the dimensions of the two operands together
+            left_operand, right_operand = context[1][:2]
+            out_arr.dimensions = (left_operand.dimensions if isinstance(left_operand, type(self)) else 0) + \
+                                 (right_operand.dimensions if isinstance(right_operand, type(self)) else 0)
+        elif ufunc_type in ['divide', 'true_divide', 'floor_divide']:
+            # The result should subtract the dimensions of the second operand from the first
+            left_operand, right_operand = context[1][:2]
+            out_arr.dimensions = (left_operand.dimensions if isinstance(left_operand, type(self)) else 0) - \
+                                 (right_operand.dimensions if isinstance(right_operand, type(self)) else 0)
+        elif ufunc_type in ['power']:
+            # The result should have its dimensions multiplied by the exponent
+            left_operand, right_operand = context[1][:2]
+            out_arr.dimensions = left_operand.dimensions * np.asarray(right_operand)
+        elif ufunc_type in ['sqrt']:
+            # The result should have halved dimension powers
+            out_arr.dimensions = context[1][0].dimensions / 2
+        elif ufunc_type in ['square']:
+            # The result should have doubled dimension powers
+            out_arr.dimensions = context[1][0].dimensions * 2
+        elif ufunc_type in ['greater', 'greater_equal', 'equal', 'less_equal', 'less', 'not_equal']:
+            # Equality checks should be dimensionless
+            out_arr.dimensions = np.zeros_like(context[1][0].dimensions)
+        else:
+            # Other ufuncs will just return the left operand's dimensions
+            out_arr.dimensions = context[1][0].dimensions
 
         return np.ndarray.__array_wrap__(self, out_arr, context)
 

tests/test_array.py
@@ -7,11 +7,19 @@ import dimpy.units
 class array_tests(unittest.TestCase):
     def test_repr(self):
         # Check that the array repr matches intended output
-        self.assertEquals(repr(dimpy.data([1.0, 2.0, 3.0], units='N')), '[ 1.  2.  3.] - dimensions: [mass: 1, length: 1, time: -2, current: 0, temperature: 0, amount of substance: 0, luminous intensity: 0]')
+        self.assertEquals(repr(dimpy.data([1.0, 2.0, 3.0], units='N')),
+                          '[ 1.  2.  3.] - dimensions: [mass: 1, length: 1, time: -2, current: 0, temperature: 0, amount of substance: 0, luminous intensity: 0]')
 
     def test_powers(self):
         # Check that arrays scale properly to the native internal representation
-        self.assertTrue(np.all(dimpy.data([1.0, 2.0, 3.0], units='mm') == dimpy.data([1e-3, 2e-3, 3e-3], units='m')))
+        np.testing.assert_array_almost_equal(dimpy.data([1.0, 2.0, 3.0], units='mm'), dimpy.data([1e-3, 2e-3, 3e-3], units='m'))
+
+    def test_scalars(self):
+        # Check that we can grab a single element from an array
+        lengths = dimpy.data([1.0, 2.0, 3.0], units='m')
+        self.assertEqual(lengths[0], dimpy.data([1.0], units='m'))
+        self.assertEqual(lengths[1], dimpy.data([2.0], units='m'))
+        self.assertEqual(lengths[2], dimpy.data([3.0], units='m'))
 
     def test_dimensional_homogeneity(self):
         # Check that you cannot add dissimilar dimensions
@@ -36,19 +44,25 @@ class array_tests(unittest.TestCase):
         acceleration = dimpy.data([1.0], units='m/s^2')
         force = dimpy.data([1.0], units='N')
 
-        self.assertTrue(mass * acceleration == force)
-        self.assertTrue(force / acceleration == mass)
-        self.assertTrue(force / mass == acceleration)
+        np.testing.assert_array_almost_equal(mass * acceleration, force)
+        np.testing.assert_array_almost_equal(force / acceleration, mass)
+        np.testing.assert_array_almost_equal(force / mass, acceleration)
 
         # Check that taking the square root halves the dimensions, and that squaring doubles them
         area = dimpy.data([100.0], units='m^2')
         side = dimpy.data([10.0], units='m')
 
-        self.assertTrue(np.sqrt(area) == side)
-        self.assertTrue(np.square(side) == area)
+        np.testing.assert_array_almost_equal(np.sqrt(area), side)
+        np.testing.assert_array_almost_equal(np.square(side), area)
 
         # Check that raising a value to a power handles the dimensions properly
-        length = dimpy.data([1.0], units='cm')
-        volume = dimpy.data([1.0], units='mL')
+        length = dimpy.data([0.1, 1.0, 10.0], units='m')
+        volume = dimpy.data([1.0, 1000.0, 1000000.0], units='L')
 
-        self.assertTrue(length**3 == volume)
+        np.testing.assert_array_almost_equal(length**3, volume)
+
+        # Check other equality behavior
+        self.assertTrue(dimpy.data([1.0], units='mm') < dimpy.data([1.0], units='m'))
+        self.assertTrue(dimpy.data([1.0], units='mm') <= dimpy.data([1.0], units='m'))
+        self.assertTrue(dimpy.data([1.0], units='m') > dimpy.data([1.0], units='mm'))
+        self.assertTrue(dimpy.data([1.0], units='m') >= dimpy.data([1.0], units='mm'))