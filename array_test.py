6462983081f71d15d0f32b0ee8323b47e60434f3
Matt Fulghum <mfulghum@gmail.com>
2016-06-17 10:27:26 -0700

    Add a function to find all valid dimensional sets

--- array_test.py
+++ array_test.py
@@ -25,8 +25,7 @@ L = dimpy.unit('m')
 g = dimpy.unit('m/s^2')
 
 variables = [T, M, L, g]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-print(dimpy.analysis.get_dimensional_set_shape(variables))
+print(dimpy.analysis.get_valid_dimensional_sets(variables))
 
 # Black body model
 u = dimpy.unit('kg/(m^2-s^2)')
@@ -36,79 +35,6 @@ c = dimpy.unit('m/s')
 kB = dimpy.unit('kg-m^2/(s^2-K)')
 T = dimpy.unit('K')
 
-
 variables = [u, wavelength, h, c, kB, T]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-print(dimensions)
-print(dimpy.analysis.get_dimensional_set_shape(variables))
+print(dimpy.analysis.get_valid_dimensional_sets(variables))
 
-num_variables, num_dimensions, num_groups = dimpy.analysis.get_dimensional_set_shape(variables)
-#permutations = [[(x+y) % num_variables for x in range(num_variables)][num_groups:] for y in range(num_variables)]
-
-permutations = list({tuple(set(value[num_groups:])) for value in itertools.permutations(range(num_variables))})
-dimensions = np.vstack([variable.dimensions for variable in variables])
-active_dimensions = np.where(np.sum(dimensions != 0, axis=0) != 0)[0]
-
-for permutation in permutations:
-    dependent_matrix = dimensions[np.ix_(permutation, active_dimensions)]
-    try:
-        assert dependent_matrix.shape[0] == dependent_matrix.shape[1]
-        assert np.linalg.det(dependent_matrix) != 0
-        assert np.linalg.matrix_rank(dependent_matrix) == dependent_matrix.shape[0]
-    except:
-        continue
-    
-    print('permutation: %s' % repr(permutation))
-    print(dependent_matrix)
-    print('rank: %d' % np.linalg.matrix_rank(dependent_matrix.T))
-    print('determinant: %d' % np.linalg.det(dependent_matrix.T))
-    print('')
-
-
-variables = [u, wavelength, h, c]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-dimensional_set = np.where(np.sum(dimensions != 0, axis=0) != 0)
-A = dimensions[:,dimensional_set[0]]
-#print(np.linalg.inv(A))
-#print(np.linalg.det(A))
-print(np.linalg.matrix_rank(A))
-
-variables = [wavelength, h, c, kB]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-dimensional_set = np.where(np.sum(dimensions != 0, axis=0) != 0)
-A = dimensions[:,dimensional_set[0]]
-print(np.linalg.inv(A))
-#print(np.linalg.det(A))
-print(np.linalg.matrix_rank(A))
-
-variables = [h, c, kB, T]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-dimensional_set = np.where(np.sum(dimensions != 0, axis=0) != 0)
-A = dimensions[:,dimensional_set[0]]
-print(np.linalg.inv(A))
-#print(np.linalg.det(A))
-#print(np.linalg.matrix_rank(A))
-
-variables = [c, kB, T, u]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-dimensional_set = np.where(np.sum(dimensions != 0, axis=0) != 0)
-A = dimensions[:,dimensional_set[0]]
-print(np.linalg.inv(A))
-#print(np.linalg.det(A))
-#print(np.linalg.matrix_rank(A))
-
-variables = [kB, T, u, wavelength]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-dimensional_set = np.where(np.sum(dimensions != 0, axis=0) != 0)
-A = dimensions[:,dimensional_set[0]]
-print(np.linalg.inv(A))
-#print(np.linalg.det(A))
-#print(np.linalg.matrix_rank(A))
-
-variables = [T, u, wavelength, h]
-dimensions = np.vstack([variable.dimensions for variable in variables])
-dimensional_set = np.where(np.sum(dimensions != 0, axis=0) != 0)
-A = dimensions[:,dimensional_set[0]]
-print(np.linalg.inv(A))
-#print(np.linalg.det(A))
-#print(np.linalg.matrix_rank(A))
--- dimpy/analysis.py
+++ dimpy/analysis.py
@@ -3,8 +3,10 @@ Dimensional analysis functions
 Copyright 2016, Matthew Fulghum [mfulghum]
 
 15 June 2016 - Initial commit.
+17 June 2016 - Add a function to find all valid dimensional sets
 """
 
+import itertools
 import numpy as np
 
 def get_dimensional_set_shape(variables):
@@ -15,3 +17,53 @@ def get_dimensional_set_shape(variables):
     num_groups = num_variables - np.linalg.matrix_rank(dimensions.T)
 
     return int(num_variables), int(num_dimensions), int(num_groups)
+
+def get_valid_dimensional_sets(variables):
+    num_variables, num_dimensions, num_groups = get_dimensional_set_shape(variables)
+    permutations = list({tuple(set(value[num_groups:])) for value in itertools.permutations(range(num_variables))})
+    dimensions = np.vstack([variable.dimensions for variable in variables])
+    active_dimensions = np.where(np.sum(dimensions != 0, axis=0) != 0)[0]
+
+    valid_sets = []
+
+    for permutation in permutations:
+        dependent_variables = tuple(set(range(num_variables)) - set(permutation))
+        independent_variables = tuple(permutation)
+
+        dependent_matrix = dimensions[np.ix_(dependent_variables, active_dimensions)]
+        independent_matrix = dimensions[np.ix_(independent_variables, active_dimensions)]
+
+        try:
+            assert independent_matrix.shape[0] == independent_matrix.shape[1]
+            assert np.linalg.det(independent_matrix) != 0
+            assert np.linalg.matrix_rank(independent_matrix) == independent_matrix.shape[0]
+        except:
+            continue
+
+        pi_matrix = np.vstack(
+            [np.eye(num_groups), -np.linalg.inv(np.matrix(independent_matrix.T)) * np.matrix(dependent_matrix.T)])
+
+        variable_order = dependent_variables + independent_variables
+        idx = tuple(
+            [value[1] for value in sorted(zip(variable_order, range(len(variable_order))), key=lambda value: value[0])])
+        pi_matrix = pi_matrix[np.ix_(idx, range(num_groups))]
+
+        if not np.all(dimensions.T * pi_matrix == 0):
+            continue
+
+        u, s, vh = np.linalg.svd(pi_matrix.T)
+        tol = max(1e-13, 1e-12 * s[0])
+        rank = (s >= tol).sum()
+        nullspace = vh[rank:].conj().T
+
+        in_valid_sets = False
+        for (_, valid_rank, valid_nullspace) in valid_sets:
+            in_valid_sets |= (rank == valid_rank) and np.allclose(nullspace, valid_nullspace)
+        if not in_valid_sets:
+            valid_sets.append((pi_matrix, rank, nullspace))
+            
+            print('permutation: %s' % repr(permutation))
+            print(pi_matrix)
+            print(nullspace)
+
+    return [valid_set[0] for valid_set in valid_sets]
